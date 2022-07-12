880034feefa5eca3321d485062c461ef59747f26
sumitra05 <sumitra9039sumi@gmail.com>
2022-07-12 20:52:44 +0530

    TODO

---
+++ src/components/TaskList.jsx
@@ -0,0 +1,48 @@
+import React , {useState} from 'react'
+import {v4 as uuid} from "uuid";
+import "./TaskList.css"
+
+const TaskList = () => {
+    const [item, setItem] = useState("");
+    const [list, setList] = useState([]);
+
+     const addItem = (e) =>{
+        //  console.log(e.target.value)
+      setItem(e.target.value);
+    //   console.log(item)
+     }
+
+     const addList = () =>{
+        //  console.log(item)
+         const obj = {items:item, key : uuid()}
+         setList([...list, obj])
+        //  console.log(list)
+         setItem("")
+     }
+
+     const del = (key) =>{
+      setList(list.filter((e)=>e.key !== key))
+
+     }
+  return (
+    <>
+    <div className='nav'>
+     <h1>TODO WEB APP</h1>
+    </div>
+    <h3>Tasks List</h3>
+    <input type="text" placeholder='Enter your Task' value = {item} onChange={(e)=> addItem(e)}/>
+       <button onClick={addList}>Add Task</button>
+       <div>
+       {
+           list.map(({items,key})=>
+           <div key={key}>{items}
+             <button onClick={() =>del(key)}>Delete</button>
+           </div>
+           )
+       }
+       </div>
+    </>
+  )
+}
+
+export default TaskList;
